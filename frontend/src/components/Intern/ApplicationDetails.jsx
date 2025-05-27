a902078c796105e40aec359e683d85d3e5212c50
lenovo <akramaznag96@gmail.com>
2025-05-27 13:15:56 +0200

    updated

---
+++ frontend/src/components/Intern/ApplicationDetails.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { IdentificationIcon,PaperAirplaneIcon ,DocumentDuplicateIcon,DocumentIcon} from '@heroicons/react/24/outline';
+import { Link } from 'react-router-dom';
+
+export default function ApplicationDetails() {
+  return (
+      <div className='flex flex-col h-auto gap-y-3  border-blue rounded-lg'>
+            
+             <div className='bg-gray-200 rounded-lg w-full h-[60px]  flex justify-center items-center !px-5 gap-x-1'>
+                 <div className='text-2xl font-bold uppercase w-full bg-blue h-[100%] flex justify-start items-center gap-x-3'>
+                  <span className='text-blue-500'>Afficher et gérer les détails</span>
+                </div>
+             </div>
+             {/* application container */}
+             <div className='bg-gray-200 h-auto w-full flex flex-col gap-y-3 !p-5 rounded-lg border-1 border-gray-300'>
+                   {/* //application */}
+                    <div className='bg-white h-[200px] w-full !p-4 flex gap-5 shadow-lg rounded-lg '>
+                            <div className='w-full flex flex-col '>
+                                <div className='w-full flex justify-between !mb-1'>
+                                  <div className='bg-blue-200 text-blue-800 text-[12px] capitalize font-bold w-[7%] flex justify-center items-center rounded-md border-none'>postulé</div>
+                                </div>
+                                <Link to={'/stage/titre-de-stage/'} className='text-lg font-bold capitalize hover:underline '> Financial Sales Representative</Link>
+                                <div className='text-md font-light capitalize'>sanlam</div>
+                                <div className='text-md font-light capitalize'>casablanca</div>
+                                <div className='capitalize text-md font-light'>envoye le 11-11-2025</div>
+                            </div>
+
+                    </div>
+                   {/* //application infos */}
+                   <div className='bg-white h-auto w-full !p-4 flex flex-col gap-5 shadow-lg rounded-lg '>
+                            <div className='w-full flex flex-col gap-y-1'>
+                                <Link className='text-lg font-bold capitalize'>Détails de la candidature</Link>
+                                <div className='text-sm font-bold capitalize text-slate-500'>Coordonnées</div>
+                            </div>
+                            <div className='flex flex-col items-start justify-start  h-auto w-full border-1 border-gray-200 rounded-lg'>
+                                <div className='bg-red w-full !p-5 border-b-1 border-gray-200  flex flex-col gap-y-1 '>
+                                    <div className='text-sm text-slate-500 capitalize'>le nom complet</div>
+                                    <div className='text-md font-bold capitalize'>ilam amir</div>
+                                </div>
+                                <div className='bg-red w-full !p-5 border-b-1 border-gray-200  flex flex-col gap-y-1 '>
+                                    <div className='text-sm text-slate-500 capitalize'>Email</div>
+                                    <div className='text-md font-bold capitalize'>amir@gmail.com</div>
+                                </div>
+                                <div className='bg-red w-full !p-5 border-b-1 border-gray-200  flex flex-col gap-y-1 '>
+                                    <div className='text-sm text-slate-500 capitalize'>Ville</div>
+                                    <div className='text-md font-bold capitalize'>Casablanca</div>
+                                </div>
+                                  <div className='bg-red w-full !p-5 b   flex flex-col gap-y-1 '>
+                                    <div className='text-sm text-slate-500 capitalize'>téléphone</div>
+                                    <div className='text-md font-bold capitalize'>0654339871</div>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <div className='uppercase text-md text-slate-500 font-bold !mb-2'>cv</div>
+                                <div className='flex items-center gap-x-5'>
+                                        <div className='w-[100%] !px-2 !py-3 rounded-lg border-1 border-gray-200 flex items-center gap-x-5 shadow-md '>
+                                            <DocumentIcon className='w-6 h-6 text-blue-500' />
+                                            <a href="/download.png" download="cv.png" className="text-blue-500">cv.pdf</a>
+                                                                            
+                                        </div>                                   
+                                </div>
+
+                            </div>
+
+                    </div>
+
+                
+
+                    
+                  
+             </div>
+             {/* no applications */}
+               {/* <div className='bg-gray-200 h-auto w-full flex justify-center items-center !p-5 rounded-lg border-1 border-gray-300 min-h-[200px]'>
+                    <div className='bg-red w-[50%] h-[200px] flex flex-col items-center gap-y-2'>
+                        <div className='bg-blue-200 h-[70px] w-[70px] rounded-full flex justify-center items-center'><PaperAirplaneIcon className='w-6 h-6 rotate-270 text-blue-600'/> </div>
+                        <div className='text-lg capitalize font-bold'>Aucune candidature trouvée</div>
+                        <p className='text-sm text-center text-slate-700 first-letter:capitalize'>Essayez d'obtenir un entretien de Stage en envoyant des candidatures à des offres de Stage</p>
+                        <Link className=' w-[45%] h-[25%] !py-1 !px-2 flex gap-x-3 justify-center items-center bg-blue-600 text-white rounded-lg'>
+                           <DocumentDuplicateIcon className='w-7 h-7 '/> <div className='font-bold text-md capitalize'>touver mon stage</div>
+                        </Link>
+                    </div>                 
+             
+                </div>  */}
+     
+         </div>
+  )
+}
--- frontend/src/components/InternshipDetails.jsx
+++ frontend/src/components/InternshipDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {CheckIcon,MapPinIcon,CloudIcon,BriefcaseIcon,DocumentIcon, ClockIcon,PencilSquareIcon} from '@heroicons/react/24/outline';
+import {CheckIcon,MapPinIcon,CloudIcon,BriefcaseIcon,DocumentIcon, ClockIcon,PencilSquareIcon,CheckCircleIcon,XMarkIcon} from '@heroicons/react/24/outline';
 import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
 import { useState,useEffect ,useRef} from 'react';
@@ -33,8 +33,24 @@ export default function InternshipDetails() {
 
     const [isOpen, setIsOpen] = useState(false);
     const [isVisible,setIsVisible]=useState(false);
-    const [cvFile, setCvFile] = useState(null);
+    const [notification,setNotification]=useState(false)
 
+    const [cvFile, setCvFile] = useState(null);
+    const IfNotification=()=>{
+     
+        setNotification(true)
+    }
+    useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        setNotification(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
+    const CloseNotification=()=>{
+        setNotification(false)
+    }
     const OpenPopUp=()=>{
         setIsOpen(true)
         setTimeout(() => {
@@ -57,12 +73,39 @@ export default function InternshipDetails() {
         'Étudiant(e) en Finance ou tout autre domaine connexe',
         'Étudiant(e) en Finance ou tout autre domaine connexe',
     ]
+    const OnSubmit=(e)=>{
+        e.preventDefault()
+        ClosePopUp()
+        IfNotification()
+    }
 
   return (
 <div className='flex justify-center'>
       {/* Container wrapper */}
       <div className='!my-[130px]  w-[85%] '>
         <div className='bg-red h-auto  w-full flex gap-x-3 '>
+            {/* notification */}
+             <div className={`fixed right-0 top-0 w-[30%] h-auto flex justify-end transition-all duration-500 ease-in-out
+    ${notification ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5 pointer-events-none'}`}>
+                <div className={ 'bg-white  w-[80%] h-[75px] border-1 border-gray-200 rounded-lg relative right-4 top-4 shadow-md flex items-center justify-between !p-3 gap-x-2 transition-opacity duration-300 '}>
+                  
+                  <div className='flex items-center gap-x-2 '>
+
+                    <CheckCircleIcon className='w-7 h-7 text-green-400 relative bottom-2'/>
+                    <div className='flex flex-col gapy-y-2'>
+                        <div className='first-letter:capitalize font-bold text-sm'>candidature envoyé</div>
+                        <div className='text-[12px] first-letter:capitalize text-slate-500'>candidature envoyé avec success</div>
+                    </div>
+                  </div>
+                    
+                  <div className='flex items-center gap-x-2 '>
+
+                    <XMarkIcon onClick={()=>CloseNotification()} className='w-6 h-6 text-slate-500 relative bottom-2 font-bold'/>
+                   
+                  </div>
+
+                </div>
+            </div>
             {/* apply popup */}
              {isOpen && (
                   <div    className={`fixed inset-0 z-[60] flex justify-center items-center bg-black/50 transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} >
@@ -77,7 +120,7 @@ export default function InternshipDetails() {
                         </div>
                         <div className='bg-red !p-5 h-auto w-full border-1 border-slate-300 rounded-lg' > 
                             <h1 className='text-lg capitalize font-semibold'>merci de verifier votre candidature</h1>
-                            <form className='w-full flex flex-col gap-y-2'>
+                            <form className='w-full flex flex-col gap-y-2' onSubmit={(e)=>OnSubmit(e)}>
                                 <div className='flex flex-col gap-y-2 w-full'>
                                      <label className='text-sm capitalize text-slate-500'>le nom complet</label>
                                      <div className='w-full flex items-center gap-x-5'>
